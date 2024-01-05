0b2ef3bff03edc79f945074bc7321271e6b69634
David Moser <david.moser@lacewingprime.com>
2024-01-05 17:53:38 -0500

    Added conditionals to control player bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,31 +12,24 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //take the starting position = new position (0, 0, 0)
-        transform.position = _startPosition;
+        transform.position = _startPosition;//set start pos to (0, 0, 0)
     }
 
     // Update is called once per frame
     void Update()
     {
-        //pull Inputs from the Unity Project Settings editor Input settings into a variable
-        //so that we can use a controller or keyboard inputs to handle player movement
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        //This commented out Translate code uses two "new" calls; one per Vector3,
-        //and it's cluttered, but it works perfectly.
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
-        //optimized for performance (only one Vector3 "new" call) and readability
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(_speed * Time.deltaTime * direction);
 
-
-        //if player postion on y-axis is greater than 0, then position = 0
 
         if (transform.position.y >= 0)
         {
@@ -46,8 +39,6 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
-
-        //if player on x-axis > 11, x = -11, else if x < -11, x = 11
         if (transform.position.x >= 11)
         {
             transform.position = new Vector3(-11, transform.position.y, 0);
